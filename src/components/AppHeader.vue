646f6e69f5d15ffbab39189cfb3649de738bdd59
MarcoColloca <marco.colloca3511@gmail.com>
2024-04-22 15:49:42 +0200

    Feat → moved AppTicket logic into single component

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,11 @@
 <script>
     import SocialIcons from './single-components/SocialIcons.vue';
+    import AppTicket from './single-components/AppTicket.vue'
 
     export default {
         components:{
             SocialIcons,
+            AppTicket,
         },
 
         data(){
@@ -48,14 +50,9 @@
     <!-- Page Header -->
     <header>
         <!-- Header Ticket Section -->
-        <div class="header__ticket">
-            <p class="header__ticket__intro">Starts TOMORROW! Our biggest event of the year...</p>
-            <div class="header__ticket__timer">
-                <span class="header__ticket__timer--icon"> <font-awesome-icon :icon="['far', 'clock']"/> </span>  
-                <span class="header__ticket__timer--time"> {{timerTime.days}} : {{timerTime.hours}} : {{timerTime.minutes}} : {{timerTime.seconds}} </span>
-            </div>
-            <button>Get Ticket</button>
-        </div>
+        <AppTicket
+         :timer="this.timerTime"
+        ></AppTicket>
 
         <!-- Header Navbar Section -->
         <div class="header__navbar">
@@ -92,31 +89,6 @@
     @use '../assets/style/partials/variables.scss' as *;
     @use '../assets/style/partials/mixins.scss' as *;
 
-    /* Ticket Section*/
-    .header__ticket{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        gap: 50px;
-        background-color: $app-lightgray;
-        padding: 10px 0;
-        .header__ticket__intro{
-            font-size: 14px;
-        }
-        .header__ticket__timer{
-            font-size: 18px;
-            font-weight: bold;
-            color: #3F3A64;
-            .header__ticket__timer--icon{
-                margin-right: 10px
-            }
-        }
-        
-        button{
-            @include btn-artist;
-            padding: 8px 40px;
-        }
-    }
 
 
     /* Navbar Section */
---
+++ src/components/single-components/AppTicket.vue
@@ -0,0 +1,60 @@
+<script>
+    export default {
+        props:{
+            timer: Object,
+        },
+
+        data(){
+            return{
+
+            }
+        }
+    }
+</script>
+
+
+<template>
+    <div class="header__ticket">
+        <p class="header__ticket__intro">Starts TOMORROW! Our biggest event of the year...</p>
+        <div class="header__ticket__timer">
+            <span class="header__ticket__timer--icon"> <font-awesome-icon :icon="['far', 'clock']"/> </span>  
+            <span class="header__ticket__timer--time"> {{timer.days}} : {{timer.hours}} : {{timer.minutes}} : {{timer.seconds}} </span>
+        </div>
+        <button>Get Ticket</button>
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+    @use '../../assets/style/partials/variables.scss' as *;
+    @use '../../assets/style/partials/mixins.scss' as *;
+
+
+    /* Ticket Section*/
+    .header__ticket{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 50px;
+        background-color: $app-lightgray;
+        padding: 10px 0;
+        .header__ticket__intro{
+            font-size: 14px;
+        }
+        .header__ticket__timer{
+            font-size: 18px;
+            font-weight: bold;
+            color: #3F3A64;
+            .header__ticket__timer--icon{
+                margin-right: 10px
+            }
+        }
+        
+        button{
+            @include btn-artist;
+            padding: 8px 40px;
+        }
+    }
+
+</style>
