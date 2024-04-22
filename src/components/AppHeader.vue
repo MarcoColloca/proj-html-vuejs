e428a5d01c62153966abd96be9ac6c7040d3e6a7
MarcoColloca <marco.colloca3511@gmail.com>
2024-04-22 16:04:32 +0200

    Feat → moved Header Navbar Logic to single component

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,11 @@
 <script>
-    import SocialIcons from './single-components/SocialIcons.vue';
-    import AppTicket from './single-components/AppTicket.vue'
+    import AppTicket from './single-components/AppTicket.vue';
+    import AppNavbar from './single-components/AppNavbar.vue'
 
     export default {
-        components:{
-            SocialIcons,
+        components:{            
             AppTicket,
+            AppNavbar,
         },
 
         data(){
@@ -51,35 +51,14 @@
     <header>
         <!-- Header Ticket Section -->
         <AppTicket
-         :timer="this.timerTime"
+         :timer="timerTime"
         ></AppTicket>
 
         <!-- Header Navbar Section -->
-        <div class="header__navbar">
-            <div class="container">
-                <div class="row">
-                    <div class="col-3">
-                        <div class="header__navbar__logo">
-                            <img :src="logoSrc" alt="Logo">
-                        </div>
-                    </div>
-    
-                    <div class="col-6">                        
-                        <ul class="header__navbar__tabs">
-                            <li v-for="tab in navbarTabs">
-                                <a :href="tab.href">
-                                    {{ tab.name }} <font-awesome-icon :icon="['fas', 'chevron-down']" class="header__navbar__tabs__icon"/>
-                                </a> 
-                            </li>
-                        </ul>                            
-                    </div>
-    
-                    <div class="col-3">
-                       <SocialIcons></SocialIcons>                    
-                    </div>    
-                </div>
-            </div>
-        </div>
+        <AppNavbar
+         :logo="logoSrc"
+         :tabs="navbarTabs"
+        ></AppNavbar>
     </header>
 </template>
 
@@ -91,33 +70,4 @@
 
 
 
-    /* Navbar Section */
-    .header__navbar{
-        padding: 30px 0;
-        
-        .row{
-            justify-content: space-between;
-            align-items: center;
-        }
-        
-
-        .header__navbar__logo{
-            img{
-                height: 30px;
-            }
-        }
-
-        .header__navbar__tabs{
-            display: flex;
-            justify-content: center;
-            li{
-                padding: 0 5px;
-                margin-right: 10px;
-                .header__navbar__tabs__icon{
-                    font-size: 10px;
-                    margin-top: -15px;
-                }
-            }
-        }
-    }
 </style>
---
+++ src/components/single-components/AppNavbar.vue
@@ -0,0 +1,94 @@
+<script>
+    import SocialIcons from './SocialIcons.vue';
+
+
+    export default {
+        components:{
+            SocialIcons,
+        },
+        props:{
+            logo: String,
+            tabs: Array,
+        },
+
+        data(){
+            return{
+
+            }
+        }
+    }
+</script>
+
+
+
+
+<template>
+    <div class="header__navbar">
+        <div class="container">
+            <div class="row">
+                <div class="col-3">
+                    <div class="header__navbar__logo">
+                        <img :src="logo" alt="Logo">
+                    </div>
+                </div>
+
+                <div class="col-6">                        
+                    <ul class="header__navbar__tabs">
+                        <li v-for="tab in tabs">
+                            <a :href="tab.href">
+                                {{ tab.name }} <font-awesome-icon :icon="['fas', 'chevron-down']" class="header__navbar__tabs__icon"/>
+                            </a> 
+                        </li>
+                    </ul>                            
+                </div>
+
+                <div class="col-3">
+                    <SocialIcons></SocialIcons>                    
+                </div>    
+            </div>
+        </div>
+    </div>
+
+</template>
+
+
+
+
+<style lang="scss" scoped>
+
+    @use '../../assets/style/partials/variables.scss' as *;
+    @use '../../assets/style/partials/mixins.scss' as *;
+
+
+    /* Navbar Section */
+    .header__navbar{
+        padding: 30px 0;
+        
+        .row{
+            justify-content: space-between;
+            align-items: center;
+        }
+        
+
+        .header__navbar__logo{
+            img{
+                height: 30px;
+            }
+        }
+
+        .header__navbar__tabs{
+            display: flex;
+            justify-content: center;
+            li{
+                padding: 0 5px;
+                margin-right: 10px;
+                .header__navbar__tabs__icon{
+                    font-size: 10px;
+                    margin-top: -15px;
+                }
+            }
+        }
+    }
+
+
+</style>
