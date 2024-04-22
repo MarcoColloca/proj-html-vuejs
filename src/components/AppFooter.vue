54a143217f3a6c3b8bce74855fbc3901cc4c4d96
MarcoColloca <marco.colloca3511@gmail.com>
2024-04-22 17:38:42 +0200

    Feat → implemented FooterInfo component into AppFooter

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -10,6 +10,67 @@
 
         data(){            
             return {
+                footerAddress:{
+                    title: 'Address',
+                    infos: ['382 NE 191st St # 87394 Miami, FL 33179-3899', '+1(305) 547-9909 (91m - 5pm EST, Monday - Friday)', 'support@maxcoach.com']
+                },
+
+                footerExplore:{
+                    title: 'Explore',
+                    infos: [
+                        {
+                            name: 'Start here',
+                            href: '#'
+                        }, 
+                        {
+                            name: 'Success',
+                            href: '#'
+                        }, 
+                        {
+                            name: 'Story',
+                            href: '#'
+                        },
+                        {
+                            name: 'Blog',
+                            href: '#'
+                        }, 
+                        {
+                            name: 'Courses',
+                            href: '#'
+                        }, 
+                        {
+                            name: 'Contact Us',
+                            href: '#'
+                        }
+                    ]
+                },
+
+                footerInformations:{
+                    title: 'Informations',
+                    infos: [
+                        {
+                            name: 'Membership',
+                            href: '#'
+                        }, 
+                        {
+                            name: 'Purchase guide',
+                            href: '#'
+                        }, 
+                        {
+                            name: 'Pirvacy Policy',
+                            href: '#'
+                        }, 
+                        {
+                            name: 'Terms of services',
+                            href: '#'
+                        },                         
+                    ]
+                },
+
+                footerInstagram:{
+                    title: 'Instagram',
+                    infos: ['/imgs/artist-info-01.jpg', '/imgs/artist-info-02.jpg', '/imgs/artist-info-03.jpg']
+                },
                 
             }
         }
@@ -23,7 +84,12 @@
 
 <template>
     <div>
-        <FooterInfo></FooterInfo>
+        <FooterInfo
+         :address="footerAddress"
+         :explore="footerExplore"
+         :informations="footerInformations"
+         :instagram="footerInstagram"
+        ></FooterInfo>
 
         <FooterCopyright></FooterCopyright>
     </div>
@@ -33,8 +99,5 @@
 
 
 <style lang="scss" scoped>
-    div{
-        height: 100px;
-        background-color: peru;
-    }
+
 </style>
--- src/components/single-components/footer/FooterInfo.vue
+++ src/components/single-components/footer/FooterInfo.vue
@@ -1,18 +1,114 @@
 <script>
-    export default {
+
+    import SocialIcons from '../general/SocialIcons.vue'
+
+    export default {     
+        components:{
+            SocialIcons,
+        },
         
+        props:{
+            address: Object,
+            explore: Object,
+            informations: Object,
+            instagram: Object,
+        },
+
+        data(){
+            return{
+            }
+        }
     }
 </script>
 
 
 <template>
-    <div>
-        <h1>Info</h1>
+    <div class="footer__info">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <ul class="footer__info--address">
+                        <h2>{{ address.title }}</h2>
+                        <li v-for="info in address.infos">
+                            <p> {{ info }} </p>   
+                        </li>
+                    </ul>                    
+                    <SocialIcons></SocialIcons>
+                </div>
+
+                <div class="col-2">
+                    <h2>{{ explore.title }}</h2>
+                    <ul class="footer__info--explore">
+                        <li v-for="info in explore.infos">
+                            <a :href="info.href"> {{ info.name }} </a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col-2">
+                    <h2>{{ informations.title }}</h2>
+                    <ul class="footer__info--information">
+                        <li v-for="info in informations.infos">
+                            <a :href="info.href"> {{ info.name }} </a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col">
+                    <h2 >{{ instagram.title }} <span class="sub-title"> &commat;maxcoach</span> </h2> 
+                    <ul class="footer__info--instagram">
+                        <li v-for="info in instagram.infos">
+                            <img :src="info" alt="Foto">
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
     </div>
 </template>
 
 
 
 <style lang="scss" scoped>
+    @use '../../../assets/style/partials/variables.scss' as *;
 
+    .footer__info{
+        font-size: 12px;
+        color: $app-darkgray;
+
+        h2{
+            font-size: 15px;
+            margin-bottom: 25px;
+        }
+
+        .sub-title{ 
+            color: $app-orange;
+            font-weight: 400;
+            font-size: 12px;
+        }
+
+        .social-icons-box{
+            justify-content: flex-start;
+            margin-top: 30px;
+        }
+
+        .footer__info--address,
+        .footer__info--explore,
+        .footer__info--information,
+        .footer__info--instagram{
+            li{
+                margin-bottom: 15px;
+            }
+        }
+
+
+        .footer__info--instagram{
+            display: flex;            
+            gap: 20px;
+            img{
+                width: 130px;
+            }
+        }
+    }
 </style>
