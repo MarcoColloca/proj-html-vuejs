d8666fca873bbe6f90c63f53a18f86b73eb8dacb
MarcoColloca <marco.colloca3511@gmail.com>
2024-04-23 13:53:21 +0200

    Add → added main artist testimonial basic HTML structure

--- src/components/single-components/main/MainArtistTestimonials.vue
+++ src/components/single-components/main/MainArtistTestimonials.vue
@@ -7,13 +7,44 @@
 
 
 <template>
-    <div>
-        <h1>TESt</h1>
+    <!-- Testimonials Section -->
+    <div class="main__artist--testimonials">
+        <!-- Testimonials Title -->
+        <div class="main__artist--testimonials__title">
+            <p class="artist-sign">Testimonials</p>
+            <h1>Why do people love me?</h1>
+        </div>
+
+        <!-- Testimonials Cards -->
+        <div class="main__artist--testimonials__card-box">
+            <div class="row">
+                <div class="col-4">
+                    <div class="main__artist--testimonials__card-box__card">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Testimonials Scrollbar -->
+        <div class="main__artist--testimonials__scrollbar">
+            
+        </div>
     </div>
 </template>
 
 
 
 <style lang="scss" scoped>
+    @use '../../../assets/style/partials/variables.scss' as *;
+    @use '../../../assets/style/partials/mixins.scss' as *;
+
+    
+    .main__artist--testimonials{
+        text-align: center;
+        h1{
+            font-weight: 600;
+        }
+    }
 
 </style>
