94167a71106928c7a9d00066720c0650801a075a
MarcoColloca <marco.colloca3511@gmail.com>
2024-04-23 16:59:10 +0200

    Add → added Main Artist Blog component base static structure

--- src/components/single-components/main/MainArtistBlog.vue
+++ src/components/single-components/main/MainArtistBlog.vue
@@ -7,10 +7,21 @@
 
 
 
-<template>
-    <div>
-        Yo!
-    </div>
+<template> 
+    <!-- Artist Blog Section -->
+    <section class="main__artist--blog">
+        <div class="main__artist--blog__title">
+            <p>firma</p>
+            <h1>Titolo</h1>
+        </div>
+
+        <div class="main__artist--blog__cards"></div>
+
+
+        <div class="main__artist--blog__link">
+            Lorem, ipsum dolor.
+        </div>
+    </section>
 </template>
 
 
