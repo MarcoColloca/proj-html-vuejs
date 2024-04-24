e2fc0b71899b383214d2c629b0b970c14ee59934
MarcoColloca <marco.colloca3511@gmail.com>
2024-04-24 12:12:32 +0200

    Refactor → added dynamically generated cards in MainArtistEvents component

--- src/components/single-components/main/MainArtistEvents.vue
+++ src/components/single-components/main/MainArtistEvents.vue
@@ -12,6 +12,72 @@
                 titleBox: {
                     sign: 'Upcoming Events',
                     title: 'Let\'s Work Together',
+                },
+
+                artistCards:[
+                    {
+                        img: '/imgs/artist-event-01-250x300.jpg',
+                        date: 'NOV 22, 2020',
+                        title: 'Storitelling Workshop',
+                        details: [
+                            {
+                                icon: ['fas', 'location-dot'],
+                                text: 'Texas, US'
+                            },
+                        ],
+                    },
+                    {
+                        img: '/imgs/artist-event-02-250x300.jpg',
+                        date: 'OCT 10, 2020',
+                        title: 'Painting Art Contest 2020',
+                        details: [
+                            {
+                                icon: ['fas', 'location-dot'],
+                                text: 'NewYork, US'
+                            },
+                        ],
+                    },
+                    {
+                        img: '/imgs/artist-event-03-250x300.jpg',
+                        date: 'NOV 23, 2020',
+                        title: 'International Art Fair 2020',
+                        details: [
+                            {
+                                icon: ['fas', 'location-dot'],
+                                text: 'Hamburg, Germany'
+                            },
+                        ],
+                    },
+                    {
+                        img: '/imgs/artist-event-04-250x300.jpg',
+                        date: 'DEC 15, 2020',
+                        title: 'Street Performance: Call for Artist',
+                        details: [
+                            {
+                                icon: ['fas', 'location-dot'],
+                                text: 'Illinois, US'
+                            },
+                        ],
+                    },
+                ],
+            }
+        },
+
+
+        methods:{
+            /** Funzione che dato un valore, ritorna una classe se l'valore è maggiore di 1 
+                Qui viene usata controllando l'indice, per dare una formattazione condizionale alle cards.*/
+            addClassOrder2(index){
+                if(index > 1){
+                    return 'order-2'
+                }
+            },
+
+            /** Funzione che dato un valore, ritorna una classe se l'valore è maggiore di 1 
+                Qui viene usata controllando l'indice, per dare una formattazione condizionale alle cards.*/
+            addClassTextEnd(index){
+                if(index > 1){
+                    return 'text-end'
                 }
             }
         }
@@ -36,102 +102,27 @@
         <div class="main__artist--events__cards">
             <div class="container-sm">
                 <div class="row">
-                    <div class="col-6">
+                    <div class="col-6" v-for="(card, i) in artistCards">
                         <!-- 1° Card Events -->
                         <div class="main__artist--events__card">
-                            <div class="main__artist--events__card--header">
-                                <img src="/imgs/artist-event-01-250x300.jpg" alt="">
+                            <div class="main__artist--events__card--header" :class="addClassOrder2(i)">
+                                <img :src="card.img" alt="">
                             </div>
 
-                            <div class="main__artist--events__card--body">
+                            <div class="main__artist--events__card--body" :class="addClassTextEnd(i)">
                                 <div class="card--body__date">
-                                    NOV 22, 2020
+                                    {{card.date}}
                                 </div>
 
                                 <div class="card--body__title">
                                     <h3>
-                                        Storytelling Workshop
+                                        {{card.title}}
                                     </h3>
                                 </div>
 
                                 <div class="card--body__place">
-                                    <p><font-awesome-icon :icon="['fas', 'location-dot']" /> Texas, US</p>
+                                    <p><span v-for="detail in card.details"><font-awesome-icon :icon="detail.icon" /> {{detail.text}}</span></p>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
-    
-                    <div class="col-6">
-                        <!-- 2° Card Events -->
-                        <div class="main__artist--events__card">
-                            <div class="main__artist--events__card--header">
-                                <img src="/imgs/artist-event-02-250x300.jpg" alt="">
-                            </div>
-
-                            <div class="main__artist--events__card--body">
-                                <div class="card--body__date">
-                                    OCT 10, 2020
-                                </div>
-
-                                <div class="card--body__title">
-                                    <h3>
-                                        Painting Art Contest 2020
-                                    </h3>
-                                </div>
-
-                                <div class="card--body__place">
-                                    <p><font-awesome-icon :icon="['fas', 'location-dot']" /> New York, US</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-    
-                    <div class="col-6">
-                        <!-- 3° Card Events -->
-                        <div class="main__artist--events__card">
-                            <div class="main__artist--events__card--body text-end">
-                                <div class="card--body__date">
-                                    NOV 23, 2020
-                                </div>
-
-                                <div class="card--body__title">
-                                    <h3>
-                                        International Art Fair 2020
-                                    </h3>
-                                </div>
-
-                                <div class="card--body__place">
-                                    <p><font-awesome-icon :icon="['fas', 'location-dot']" /> Hamburg, Germany</p>
-                                </div>
-                            </div>
-
-                            <div class="main__artist--events__card--header">
-                                <img src="/imgs/artist-event-03-250x300.jpg" alt="">
-                            </div>
-                        </div>
-                    </div>
-                    
-                    <div class="col-6">
-                        <!-- 4° Card Events -->
-                        <div class="main__artist--events__card">
-                            <div class="main__artist--events__card--body text-end">
-                                <div class="card--body__date">
-                                    DEC 15, 2020
-                                </div>
-
-                                <div class="card--body__title">
-                                    <h3>
-                                        Street Performance: Call for Artist
-                                    </h3>
-                                </div>
-
-                                <div class="card--body__place">
-                                    <p><font-awesome-icon :icon="['fas', 'location-dot']" /> Illinois, US</p>
-                                </div>
-                            </div>
-
-                            <div class="main__artist--events__card--header">
-                                <img src="/imgs/artist-event-04-250x300.jpg" alt="">
                             </div>
                         </div>
                     </div>
