7386b05926facc23437d051869b67e7a26b43cf7
MarcoColloca <marco.colloca3511@gmail.com>
2024-04-24 10:15:45 +0200

    Feat → added Main Title component

---
+++ src/components/single-components/main/main-micro-components/MainTitle.vue
@@ -0,0 +1,34 @@
+
+
+<script>
+    export default {
+
+        props:{
+            titleBox: Object,
+        },
+
+        data(){
+            return{
+
+            }
+        }
+    }
+</script>
+
+
+<template>
+    <p class="artist-sign main-sign">{{ titleBox.sign }}</p>
+    <h1 class="main-title">{{ titleBox.title }}</h1>
+</template>
+
+
+
+<style lang="scss">
+
+    .main-title{
+        text-align: center;
+        margin-top: 20px;
+        margin-bottom: 35px
+    }
+
+</style>
