8be1fbe8038c43cb8ea1fa621471481bfacb71f3
MarcoColloca <marco.colloca3511@gmail.com>
2024-04-24 10:33:07 +0200

    Fix → added box with display flex to MainTitle h1 and p

--- src/components/single-components/main/main-micro-components/MainTitle.vue
+++ src/components/single-components/main/main-micro-components/MainTitle.vue
@@ -17,18 +17,28 @@
 
 
 <template>
-    <p class="artist-sign main-sign">{{ titleBox.sign }}</p>
-    <h1 class="main-title">{{ titleBox.title }}</h1>
+    <div class="title-box">
+        <p class="artist-sign main-sign">{{ titleBox.sign }}</p>
+        <h1 class="main-title">{{ titleBox.title }}</h1>
+    </div>
 </template>
 
 
 
 <style lang="scss">
+    .title-box{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 5px;
+    }
 
     .main-title{
         text-align: center;
         margin-top: 5px;
-        margin-bottom: 35px
+        margin-bottom: 35px;        
+        max-width: 620px;   
     }
 
 </style>
